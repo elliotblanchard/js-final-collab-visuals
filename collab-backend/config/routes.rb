eee132491ee8bb3d87a56b964becf2695d24496d
Elliot Blanchard <git@elliotblanchard.com>
2020-09-28 22:23:18 -0400

    Created users controller

--- collab-backend/config/routes.rb
+++ collab-backend/config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:show]
-      resources :seeds, only: [:index] 
+      resources :users, only: [:show] 
     end
   end
 
