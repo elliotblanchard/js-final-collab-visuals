ef78702f5e96dc9023f5fcf3fdddd693516f199d
Elliot Blanchard <git@elliotblanchard.com>
2020-09-29 17:19:01 -0400

    continuing JWT

--- collab-backend/app/controllers/api/v1/auth_controller.rb
+++ collab-backend/app/controllers/api/v1/auth_controller.rb
@@ -2,14 +2,14 @@ class Api::V1::AuthController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
     def create
-        user = User.find_by(email: user_login_params[:email])
+        user = User.find_by(username: user_login_params[:username])
         # user_authenticate comes from BCrypt
         if user && user.authenticate(user_login_params[:password])
             # encode token from ApplicationController
             token = encode_token({ user_id: user.id })
-            render json: { user: UserSerializer.new(user), jwt: token }, status: accepted
+            render json: { user: UserSerializer.new(user), jwt: token }, status: :accepted
         else
-            render json: { message: 'Invalid email or password' }, status: unauthorized
+            render json: { message: 'Invalid email or password' }, status: :unauthorized
         end
     end
 
--- collab-backend/config/routes.rb
+++ collab-backend/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create, :show] 
       resources :seeds, only: [:create, :destroy]
+      post '/login', to: 'auth#create'
     end
   end
 
