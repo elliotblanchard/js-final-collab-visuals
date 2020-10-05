2279efbd1980bf0f2e54867dcbab62ebb09dabc0
Elliot Blanchard <git@elliotblanchard.com>
2020-10-05 10:02:23 -0400

    Initial start at the now-playing functionality

--- collab-backend/app/models/playlist.rb
+++ collab-backend/app/models/playlist.rb
@@ -1,4 +1,13 @@
 class Playlist < ApplicationRecord
     belongs_to :seed   
           
+    @@now_playing
+
+    def now_playing
+        @@now_playing
+    end
+
+    def now_playing=(id) 
+        @@now_playing = id 
+    end    
 end
